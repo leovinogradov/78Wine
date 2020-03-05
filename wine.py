a8804c3f2e49b6e16e6135e7c4e147e2726a3377
Leo Vinogradov <leovinogradov@gmail.com>
2020-03-05 15:45:48 -0800

    mulitprocessing

--- wine.py
+++ wine.py
@@ -15,3 +15,27 @@ model == nn.train(X, Y)
 _, accuracy = model.evaluate(X, Y)
 
 print('Accuracy: %.2f' % (accuracy*100))
+
+
+"""
+
+from multiprocessing import Pool
+import yourFunctionDefs
+
+numProcesses = 3 # or however many you need
+
+p = Pool(processes=numProcesses)
+results = []
+
+for i in range(numProcesses):
+    res = p.apply_async(yourFunctionDefs.yourFunc, (arg_a, arg_b, etc, i)) # call your training function with your arguments
+    results.append(res)
+    print("started getting result for", i)
+
+for j, res in enumerate(results):
+    result = res.get() # get results asynchronously
+    print("got result", result)
+
+p.close()
+
+"""
